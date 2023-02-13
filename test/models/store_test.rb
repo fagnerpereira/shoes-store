c00c990767c5854b7492ca7a1f63cc432f8ddb0f
Fagner P. Rosa <fagnerfpr@gmail.com>
2023-02-13 13:44:07 -0300

    fix tests

--- test/models/store_test.rb
+++ test/models/store_test.rb
@@ -1,6 +1,15 @@
 require 'test_helper'
 
 class StoreTest < ActiveSupport::TestCase
+  test 'should create with name' do
+    assert Store.new(name: 'store').save
+  end
+
+  test 'should create with unique name' do
+    Store.create(name: 'storeA')
+    assert Store.new(name: 'storeB').save
+  end
+
   test 'should not save without name' do
     store = Store.new
     assert_not store.save
