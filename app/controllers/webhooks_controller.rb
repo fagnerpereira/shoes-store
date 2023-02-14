b3d2af58d972da15fc324246763103f8b897cbff
Fagner P. Rosa <fagnerfpr@gmail.com>
2023-02-14 11:25:51 -0300

    add random past dates to fill the charts

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -9,11 +9,26 @@ class WebhooksController < ApplicationController
     # is missing or databse is unavailable I would loss this data
     # 1 approach is if this line fails I enqueue in a job to perform later
     # 2 approach is put this inside a job and after saving, call ProcessJob
+    year = 2022
+    months = (6..12).to_a
+    days = (1..30).to_a
+    #
+    # Sale.create(
+    #   store: stores.first(5).sample,
+    #   product: products.sample,
+    #   quantity: (1..15).to_a.sample,
+    #   created_at: "#{year}-#{months.sample}-#{days.sample}"
+    # )
     webhook = Webhook.create!(payload: payload)
     store = Store.find_by!(name: webhook.payload['store'])
     product = Product.find_by!(name: webhook.payload['model'])
     inventory = Inventory.find_by!(store: store, product: product)
-    Sale.create!(store: store, product: product, quantity: 1)
+    Sale.create!(
+      store: store,
+      product: product,
+      quantity: 1,
+      created_at: "#{year}-#{months.sample}-#{days.sample}"
+    )
     inventory.update(quantity: webhook.payload['inventory'].to_i)
 
     # Webhooks::ProcessJob.perform_later(webhook)
