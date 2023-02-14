45d767ddcd50bf3b5b9225e8f49d782798b46db3
Fagner P. Rosa <fagnerfpr@gmail.com>
2023-02-13 22:10:34 -0300

    fix rubocop warnings in webhook flow test

--- test/integration/webhook_flow_test.rb
+++ test/integration/webhook_flow_test.rb
@@ -8,40 +8,32 @@ class ReceivedWebhookFlowTest < ActionDispatch::IntegrationTest
       quantity: 100
     )
   end
-
   test 'should create a webhook and then process it' do
-    assert_nil(
-      Sale.find_by(
-        store: stores(:store_a),
-        product: products(:product_a),
-        quantity: 1
-      )
-    )
+    assert_nil(created_sale)
     assert_difference('Webhook.count') do
-      post webhooks_url, params: {
-        'store' => stores(:store_a).name,
-        'model' => products(:product_a).name,
-        'inventory' => 99
-      }
+      post webhooks_url, params: webhook_params
     end
-
-    assert_not_nil(
-      Sale.find_by(
-        store: stores(:store_a),
-        product: products(:product_a),
-        quantity: 1
-      )
-    )
+    assert_not_nil(created_sale)
 
     @inventory.reload
 
-    assert_equal @inventory.quantity, 99
-    assert_equal(
-      Webhook.last.payload, {
-        'store' => stores(:store_a).name,
-        'model' => products(:product_a).name,
-        'inventory' => '99'
-      }
+    assert_equal(@inventory.quantity, 99)
+    assert_equal(Webhook.last.payload, webhook_params)
+  end
+
+  def created_sale
+    Sale.find_by(
+      store: stores(:store_a),
+      product: products(:product_a),
+      quantity: 1
     )
+  end
+
+  def webhook_params
+    {
+      'store' => stores(:store_a).name,
+      'model' => products(:product_a).name,
+      'inventory' => '99'
+    }
   end
 end
