55487faf98443143a1f996fc02d9c18d942b96b3
Fagner P. Rosa <fagnerfpr@gmail.com>
2023-03-05 21:28:10 -0300

    add stores crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root 'dashboard#index'
   get '/sales_by_stores', to: 'dashboard#sales_by_stores'
   get '/top_sales_by_stores', to: 'dashboard#top_sales_by_stores'
   get '/top_sales_by_products', to: 'dashboard#top_sales_by_products'
 
+  resources :stores
   resources :dashboard, only: :index
   resources :webhooks
 end
